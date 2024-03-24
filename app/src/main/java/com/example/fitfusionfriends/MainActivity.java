3005fe6a2d9019e56c6009c0191cac0086538b07
Matthew Nguyen <matthewnguyen1230@gmail.com>
2024-03-23 22:09:27 -0400

    fixed ui constraints & added images

--- app/src/main/java/com/example/fitfusionfriends/MainActivity.java
+++ app/src/main/java/com/example/fitfusionfriends/MainActivity.java
@@ -5,22 +5,39 @@ import android.view.View;
 import android.widget.EditText;
 import android.widget.Button;
 import android.os.Bundle;
-
+import android.widget.ImageView;
 import com.google.androidgamesdk.GameActivity;
 
 public class MainActivity extends GameActivity {
 
     String name;
-    float height, weight;
+    int height, weight;
 
     EditText editName;
     EditText editWeight;
     EditText editHeight;
 
-    Button button;
+    Button submitButton;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.content_home);
+
+        editName = (EditText) findViewById(R.id.editName);
+        editWeight = (EditText) findViewById(R.id.editWeight);
+        editHeight = (EditText) findViewById(R.id.editHeight);
+
+        submitButton = (Button) findViewById(R.id.submitButton);
+        submitButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                name = editName.getText().toString();
+                height = Integer.parseInt(editHeight.getText().toString());
+                weight = Integer.parseInt(editWeight.getText().toString());
+            }
+        });
+        
     }
 
     static {
