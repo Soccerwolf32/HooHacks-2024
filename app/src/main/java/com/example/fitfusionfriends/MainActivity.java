95b9069a39491f33443651ec0174a5175390dd7e
Matthew Nguyen <matthewnguyen1230@gmail.com>
2024-03-23 22:34:20 -0400

    fixed ui AGAIN

--- app/src/main/java/com/example/fitfusionfriends/MainActivity.java
+++ app/src/main/java/com/example/fitfusionfriends/MainActivity.java
@@ -5,11 +5,12 @@ import android.view.View;
 import android.widget.EditText;
 import android.widget.Button;
 import android.os.Bundle;
-import android.widget.ImageView;
 import com.google.androidgamesdk.GameActivity;
 
 public class MainActivity extends GameActivity {
-
+    static {
+        System.loadLibrary("fitfusionfriends");
+    }
     String name;
     int height, weight;
 
@@ -37,31 +38,29 @@ public class MainActivity extends GameActivity {
                 weight = Integer.parseInt(editWeight.getText().toString());
             }
         });
-        
+
     }
 
-    static {
-        System.loadLibrary("fitfusionfriends");
-    }
 
-    @Override
-    public void onWindowFocusChanged(boolean hasFocus) {
-        super.onWindowFocusChanged(hasFocus);
 
-        if (hasFocus) {
-            hideSystemUi();
-        }
-    }
-
-    private void hideSystemUi() {
-        View decorView = getWindow().getDecorView();
-        decorView.setSystemUiVisibility(
-                View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
-                        | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-                        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-                        | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
-                        | View.SYSTEM_UI_FLAG_FULLSCREEN
-        );
-    }
+//    @Override
+//    public void onWindowFocusChanged(boolean hasFocus) {
+//        super.onWindowFocusChanged(hasFocus);
+//
+//        if (hasFocus) {
+//            hideSystemUi();
+//        }
+//    }
+//
+//    private void hideSystemUi() {
+//        View decorView = getWindow().getDecorView();
+//        decorView.setSystemUiVisibility(
+//                View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+//                        | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+//                        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+//                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+//                        | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+//                        | View.SYSTEM_UI_FLAG_FULLSCREEN
+//        );
+//    }
 }
