364f8001157c776ed3b846b649c41b98b43f27b3
Jeffrey Owusu <jeffowusu017@gmail.com>
2024-03-24 00:54:43 -0400

    scrapping the stats feature

--- app/src/main/java/Friend.java
+++ app/src/main/java/Friend.java
@@ -2,72 +2,19 @@
  * Contains the data of the friend
  */
 public class Friend {
-
-    private int friendStrength;
-    private int friendEndurance;
-    private int friendCardio;
-    private int friendFlexibility;
     private int friendLevel;
     private int friendEXP;
     private int friendEXPThreshold;
 
-    private final int MAX_FRIEND_STRENGTH = 100;
-    private final int MAX_FRIEND_ENDURANCE = 100;
-    private final int MAX_FRIEND_CARDIO = 100;
-    private final int MAX_FRIEND_FLEXIBILITY = 100;
     private final int MAX_FRIEND_LEVEL = 100;
     private final int MAX_FRIEND_EXP = 100;
     private final int THRESHOLD_MULTIPLIER = 2;
 
 
     public Friend(){
-        friendStrength = 1;
-        friendEndurance = 1;
-        friendCardio = 1;
-        friendFlexibility = 1;
         friendLevel = 1;
         friendEXP = 1;
         friendEXPThreshold = 1;
-    }
-
-    public int getFriendStrength() {
-        return friendStrength;
-    }
-
-    public void setFriendStrength(int friendStrength) {
-        this.friendStrength = friendStrength;
-        if(this.friendStrength >= MAX_FRIEND_STRENGTH)
-            this.friendStrength = MAX_FRIEND_STRENGTH;
-    }
-
-    public int getFriendEndurance() {
-        return friendEndurance;
-    }
-
-    public void setFriendEndurance(int friendEndurance) {
-        this.friendEndurance = friendEndurance;
-        if(this.friendEndurance >=  MAX_FRIEND_ENDURANCE)
-            this.friendEndurance = MAX_FRIEND_ENDURANCE;
-    }
-
-    public int getFriendCardio() {
-        return friendCardio;
-    }
-
-    public void setFriendCardio(int friendCardio) {
-        this.friendCardio = friendCardio;
-        if(this.friendCardio >= MAX_FRIEND_CARDIO)
-            this.friendCardio = MAX_FRIEND_CARDIO;
-    }
-
-    public int getFriendFlexibility() {
-        return friendFlexibility;
-    }
-
-    public void setFriendFlexibility(int friendFlexibility) {
-        this.friendFlexibility = friendFlexibility;
-        if(this.friendFlexibility >= MAX_FRIEND_FLEXIBILITY)
-            this.friendFlexibility = MAX_FRIEND_FLEXIBILITY;
     }
 
     public int getFriendLevel() {
